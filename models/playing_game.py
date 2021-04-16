7d9b77a3927632b4524168e5f3f3a428256ead91
rock-007 <umair.ashraf2019@gmail.com>
2021-04-16 21:15:24 +0100

    Computer_player added

--- models/playing_game.py
+++ models/playing_game.py
@@ -5,15 +5,12 @@ from random import Random, randrange
 
 
 player_1 = Player("Umair")
-
 player_2 = Player("John")
-
-
 computer_2 =Player("Robort_02")
-
 Game_01 = Game("first_game",player_1, player_2)
 
 selection_list = ['Rock','Scissor','Lizard', 'Spock','Paper']
+
 
 
 def set_players_gesture( player_1_gesture_selection, player_2_gesture_selection):
@@ -24,13 +21,13 @@ def set_players_gesture( player_1_gesture_selection, player_2_gesture_selection)
 
 def play_game( player_1_gesture_selection, player_2_gesture_selection):
         set_players_gesture(player_1_gesture_selection, player_2_gesture_selection)
-
-
         for win_combination in Game_01.win_combination:
                 if [player_1_gesture_selection,player_2_gesture_selection] == win_combination:
                         return player_1.name
                 elif [player_2_gesture_selection, player_1_gesture_selection] == win_combination:
                         return player_2.name
+
+
 
 def set_computer_game(player_01_name,gesture_selection):
         player_3 = Player(player_01_name)
@@ -38,17 +35,17 @@ def set_computer_game(player_01_name,gesture_selection):
         select_randon_gesture()
         Game_02 = Game("first_game",player_1, computer_2)
         return Game_02
-        
+
+
+
 def select_randon_gesture():
         index_number = randrange(5)
         computer_2.gesture_selection = selection_list[index_number]
 
 
+
 def game_with_computer(player_01_name,gesture_selection):
         Game_02 = set_computer_game(player_01_name,gesture_selection)
-        print(computer_2.gesture_selection)
-        print("computer_2.gesture_selection") 
-        print(Game_02) 
 
         for win_combination in Game_02.win_combination:
                 if [gesture_selection,computer_2.gesture_selection] == win_combination:
